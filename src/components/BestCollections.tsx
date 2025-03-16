60cdf93205651d1a706e35d26a14bfad9ecbe07c
Amadou Toure <amadouelcapotoure@gmail.com>
2025-03-16 06:40:21 -0400

    minor updates

--- src/components/BestCollections.tsx
+++ src/components/BestCollections.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { ArrowRight, ChevronLeft, ChevronRight } from "lucide-react";
 import { Link } from "react-router-dom";
 import products from "../data/products.json";
